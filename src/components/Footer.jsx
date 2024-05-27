f3f94d9e2555480a7cef4a06ec27317acdfd0dcb
pour-moi <137487367+pour-moi@users.noreply.github.com>
2024-05-27 21:12:23 +0300

    changed border top to hr tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,37 +3,40 @@ import Hand from "../assets/Hand.svg";
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <div className="hand-title">
-        <img src={Hand} />
-        <p>dogather</p>
+    <>
+      <hr />
+      <div className="footer-container ">
+        <div className="hand-title">
+          <img src={Hand} />
+          <p>dogather</p>
+        </div>
+        <div className="services">
+          <ul type="none">
+            <li className="product header">Product</li>
+            <li>Docs</li>
+            <li>What's new</li>
+          </ul>
+          <ul type="none">
+            <li className="Build header">Build</li>
+            <li>Templates</li>
+            <li>API docs</li>
+            <li>Guids & tutorials</li>
+            <li>Become and affiliate</li>
+          </ul>
+          <ul type="none">
+            <li className="get_started header">Get started</li>
+            <li>Sign up</li>
+            <li>Log in</li>
+          </ul>
+          <ul type="none">
+            <li className="resources header">Resources</li>
+            <li>About us</li>
+            <li>Email us</li>
+            <li>Terms & privacy</li>
+          </ul>
+        </div>
       </div>
-      <div className="services">
-        <ul type="none">
-          <li className="product header">Product</li>
-          <li>Docs</li>
-          <li>What's new</li>
-        </ul>
-        <ul type="none">
-          <li className="Build header">Build</li>
-          <li>Templates</li>
-          <li>API docs</li>
-          <li>Guids & tutorials</li>
-          <li>Become and affiliate</li>
-        </ul>
-        <ul type="none">
-          <li className="get_started header">Get started</li>
-          <li>Sign up</li>
-          <li>Log in</li>
-        </ul>
-        <ul type="none">
-          <li className="resources header">Resources</li>
-          <li>About us</li>
-          <li>Email us</li>
-          <li>Terms & privacy</li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 }
 
